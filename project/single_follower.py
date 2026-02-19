a6c8ba1f41fb20dec423d4aee66da01189182f62
DanielBugelnig <daniel.j.bugelnig@gmail.com>
2026-02-19 14:29:22 +0100

    add: single position following continuous

--- project/single_follower.py
+++ project/single_follower.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys, time
+from threading import Thread, Lock
 
 from evdev.ecodes import ID
 
@@ -9,11 +10,16 @@ from crazyflie.bitcraze.optitrack_integration.optitrack import NatNetRigidBodyMo
 import crazyflie.core.base_utils as base
 from crazyflie.bitcraze.crazyflie import CrazyFlie
 from crazyflie.core.shared_data import State
+from crazyflie.constants import MOCAP_TX_RATE_HZ
 
 
-OFFSET_X = 1.5
+OFFSET_X = 1 # m
+UPDATE_RATE = 10  # Hz
+ID = 15
+pos = None
+lock = Lock()
 
-def get_average_position(monitor, rigid_body_id, samples=100, interval=0.01):
+def get_average_position(monitor, rigid_body_id, update_rate=UPDATE_RATE):
     """
     Get the average position of a rigid body over multiple samples.
     
@@ -22,42 +28,52 @@ def get_average_position(monitor, rigid_body_id, samples=100, interval=0.01):
         rigid_body_id: Rigid body ID to track
         samples: Number of samples to average (default: 100)
         interval: Time interval between samples in seconds (default: 0.01)
+        Update_Rate: Update rate in Hz (default: 10)
     
     Returns:
         tuple: (x, y, z) mean position, or None if no valid positions received
     """
     sum_x, sum_y, sum_z = 0.0, 0.0, 0.0
     count = 0
-    
-    for i in range(samples):
-        pos = monitor.get_position(rigid_body_id)
-        if pos is not None:
-            sum_x += pos[0]
-            sum_y += pos[1]
-            sum_z += pos[2]
+    start_time = time.time()
+    while time.time() - start_time < 1.0 / update_rate:
+        sample = monitor.get_position(rigid_body_id)
+        if sample is not None:
+            sum_x += sample[0]
+            sum_y += sample[1]
+            sum_z += sample[2]
             count += 1
+        time.sleep(1.0 / MOCAP_TX_RATE_HZ)
         #print(f"Position from NatNet RB {rigid_body_id}: {pos}, type {type(pos)}")
-        time.sleep(interval)
     
     if count > 0:
         mean_pos = (sum_x / count, sum_y / count, sum_z / count)
-        print(f"\nMean position over {count} samples: {mean_pos}")
+        #print(f"\nMean position over {count} samples: {mean_pos}")
         return mean_pos
     else:
         print("\nNo valid positions received")
         return None
 
-ID = 15
+def object_tracking(monitor, rigid_body_id, update_rate):
+    print(f"Starting object tracking for rigid body ID {rigid_body_id}")
+    global pos 
+    while True:
+            current_pos = get_average_position(monitor, rigid_body_id, update_rate=update_rate)
+            if current_pos is not None:
+                with lock:
+                    pos = current_pos
+            
+
 
 # Natnet monitor setup
 monitor = NatNetRigidBodyMonitor()
 monitor.start()
 time.sleep(5)
 # Get average position
-mean_pos = get_average_position(monitor, ID)
 
 
-
+object_tracker_thread = Thread(target=object_tracking, args=(monitor, ID, UPDATE_RATE))
+object_tracker_thread.start()
 
 
 
@@ -72,6 +88,8 @@ cf.connect(start_flying=True)
 time.sleep(5)
 while True:
     # update the follower position based on the mean position of the rigid body with offset
-    mean_pos = (mean_pos[0], mean_pos[1], mean_pos[2])
-    cf.fly(State(mean_pos[0] + OFFSET_X, mean_pos[1], mean_pos[2]))
-    
+    with lock:
+        p = pos  
+    if p is not None:
+        cf.fly(State(p[0] + OFFSET_X, p[1], p[2]))
+    time.sleep(1.0 / UPDATE_RATE)
